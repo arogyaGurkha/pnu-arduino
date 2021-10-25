17251bb1398492fb7226cb2baef8081c2c0dfb22
Arogya Kharel <kharelarogya@hotmail.com>
2021-10-25 14:58:11 +0900

    Add Lab 8

---
+++ week8/LAB7_1/LAB7_1.ino
@@ -0,0 +1,25 @@
+#include <pitches.h>
+
+int speakerPin = 57; // Pin connected
+int melody[] = { // Notes in a melody
+  NOTE_G4, NOTE_G4, NOTE_A4, NOTE_A4,
+  NOTE_G4, NOTE_G4, NOTE_E4, NOTE_E4,
+  NOTE_G4, NOTE_G4, NOTE_A4, NOTE_A4,
+  NOTE_G4, NOTE_E4, NOTE_D4, NOTE_E4,
+};
+int noteDuration = 4; // Quarter note
+
+void setup() {
+}
+
+void loop() {
+  for (int thisNote = 0; thisNote < sizeof(melody) / sizeof(int); thisNote++) {
+    // Calculate the length of nores
+    int noteLength = 1000 / noteDuration;
+    // Play tones
+    tone(speakerPin, melody[thisNote], noteLength);
+    delay(noteLength);
+    noTone(speakerPin); // Stop playing tone
+  }
+  while (true);
+}
---
+++ week8/LAB7_2/LAB7_2.ino
@@ -0,0 +1,36 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void readTemperature() {
+  // Read the value of temp sensor
+  int reading = analogRead(55);
+  // Calculate voltage from ADC value
+  float voltage = reading * 5.0 / 1024.0;
+  Serial.print(voltage);
+  Serial.print("V: ");
+  // Calculate Celsius degree as multiply voltage value of 100
+  float temp_C = voltage * 100;
+  Serial.print(temp_C);
+  Serial.print(" C, ");
+  // Calculate Fahrenheit degree
+  float temp_F = (temp_C  * 9.0 / 5.0) + 32;
+  Serial.print(temp_F);
+  Serial.println("F");
+}
+
+void readIlluminance() {
+  // Read the value of photo-resistor
+  int reading = analog(56);
+  Serial.print("ADC : ");
+  Serial.print(reading);
+  // Calculate voltage from ADC value
+  float voltage = reading * 5.0 / 1024.0;
+  Serial.print(", Voltage : ");
+  Serial.println(voltage);
+}
+
+void loop() {
+  readTemperature();
+  readIlluminance();
+}
